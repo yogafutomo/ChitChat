62235a3dc46c91d9d797edaa7407a4fdb30e9341
yogafutomo <yogafanzy20@gmail.com>
2021-02-08 21:30:55 +0700

    add database

--- app/src/main/java/com/example/chitchat/mydatabase.java
+++ app/src/main/java/com/example/chitchat/mydatabase.java
@@ -1,13 +1,40 @@
 package com.example.chitchat;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
 
-public class mydatabase {
-    public mydatabase(Context context) {
+public class mydatabase extends SQLiteOpenHelper {
+
+    private long result;
+    private static final String DBname = "data.db";
+
+    mydatabase(Context context) {
+        super(context, DBname, null, 1);
     }
 
-    public ArrayList<String> getProfile(String qry) {
+    public void onCreate(SQLiteDatabase db){
+
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+
+    public void profile(SQLiteDatabase db){
+        db.execSQL("DROP TABLE IF EXISTS profile");
+        db.execSQL("CREATE TABLE profile");
+    }
+    public long getResult() {
+        return result;
+    }
+
+    ArrayList<String> getProfile(String qry) {
+    }
+
+    boolean insertProfile(String userName, String userAge, String userGender) {
+
     }
 }
