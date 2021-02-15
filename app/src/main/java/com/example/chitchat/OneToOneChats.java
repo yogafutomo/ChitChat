a0f41a82be63204e98b4a0bb2f114346e7058bb5
yogafutomo <yogafanzy20@gmail.com>
2021-02-15 18:11:22 +0700

    Add Activity

---
+++ app/src/main/java/com/example/chitchat/OneToOneChats.java
@@ -0,0 +1,14 @@
+package com.example.chitchat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OneToOneChats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_to_one_chats);
+    }
+}
