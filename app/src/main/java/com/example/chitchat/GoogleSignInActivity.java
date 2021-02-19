8f094021a0d127afc8b36da6a663a3b727241a52
yogafutomo <yogafanzy20@gmail.com>
2021-02-19 13:51:42 +0700

    adding XML file

---
+++ app/src/main/java/com/example/chitchat/GoogleSignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.chitchat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GoogleSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_sign_in);
+    }
+}
